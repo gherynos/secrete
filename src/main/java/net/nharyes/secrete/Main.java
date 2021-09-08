9cbe272f41e65cb05e51e4e95ee582e2d677d8df
Gherynos <gherynos@naes.co>
2021-09-08 17:56:13 +0100

    Bump version: 1.0.2 → 1.0.3

--- src/main/java/net/nharyes/secrete/Main.java
+++ src/main/java/net/nharyes/secrete/Main.java
@@ -46,7 +46,7 @@ public final class Main {  // NOPMD
     /*
      * Version
      */
-    public static final String VERSION = "1.0.2";
+    public static final String VERSION = "1.0.3";
 
     /*
      * Constants
